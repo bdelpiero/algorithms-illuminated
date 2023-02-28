99816368f713ac89fe29947287fc90984ea936b1
Bruno Del Piero <bruno.delpiero@bluealba.com>
2023-02-28 10:36:57 -0300

    mergeSort implementation

--- src/Part1/algoritms/mergeSort.test.ts
+++ src/Part1/algoritms/mergeSort.test.ts
@@ -1,34 +1,39 @@
-import { mergeSort } from "./mergeSort"
+import { mergeSort } from "./mergeSort";
 
 describe("mergeSort", () => {
   it("should return an empty array when given an empty array", () => {
-    const arr: number[] = []
-    expect(mergeSort(arr)).toEqual([])
-  })
+    const arr: number[] = [];
+    expect(mergeSort(arr)).toEqual([]);
+  });
 
   it("should return the original array when given an array with one element", () => {
-    const arr: number[] = [1]
-    expect(mergeSort(arr)).toEqual([1])
-  })
+    const arr: number[] = [1];
+    expect(mergeSort(arr)).toEqual([1]);
+  });
 
-  //   it("should sort an array of numbers in ascending order", () => {
-  //     const arr: number[] = [4, 2, 7, 1, 3]
-  //     expect(mergeSort(arr)).toEqual([1, 2, 3, 4, 7])
-  //   })
+  it("should sort an array of n numbers (where n is even) in ascending order", () => {
+    const arr: number[] = [4, 2, 7, 1, 3, 5];
+    expect(mergeSort(arr)).toEqual([1, 2, 3, 4, 5, 7]);
+  });
 
-  //   it("should return the same result for an already sorted array", () => {
-  //     const arr: number[] = [1, 2, 3, 4, 7]
-  //     expect(mergeSort(arr)).toEqual([1, 2, 3, 4, 7])
-  //   })
+  it("should sort an array of n numbers (where n is uneven) in ascending order", () => {
+    const arr: number[] = [4, 2, 7, 1, 3];
+    expect(mergeSort(arr)).toEqual([1, 2, 3, 4, 7]);
+  });
 
-  //   it("should sort an array of strings in alphabetical order", () => {
-  //     const arr: string[] = ["banana", "apple", "pear", "cherry"]
-  //     expect(mergeSort(arr)).toEqual(["apple", "banana", "cherry", "pear"])
-  //   })
+  it("should return the same result for an already sorted array", () => {
+    const arr: number[] = [1, 2, 3, 4, 7];
+    expect(mergeSort(arr)).toEqual([1, 2, 3, 4, 7]);
+  });
 
-  //   it("should not modify the original array", () => {
-  //     const arr: number[] = [4, 2, 7, 1, 3]
-  //     mergeSort(arr)
-  //     expect(arr).toEqual([4, 2, 7, 1, 3])
-  //   })
-})
+  it("should sort an array of strings in alphabetical order", () => {
+    const arr: string[] = ["banana", "apple", "pear", "cherry"];
+    expect(mergeSort(arr)).toEqual(["apple", "banana", "cherry", "pear"]);
+  });
+
+  it("should not modify the original array", () => {
+    const arr: number[] = [4, 2, 7, 1, 3];
+    mergeSort(arr);
+    expect(arr).toEqual([4, 2, 7, 1, 3]);
+  });
+});
--- src/Part1/algoritms/mergeSort.ts
+++ src/Part1/algoritms/mergeSort.ts
@@ -1,4 +1,53 @@
 export function mergeSort<T>(arr: T[]): T[] {
-  const sortedArr: T[] = []
-  return sortedArr
+  if (arr.length <= 1) return arr;
+
+  const mid = Math.floor(arr.length / 2);
+  const firstHalf = mergeSort(arr.slice(0, mid));
+  const secondHalf = mergeSort(arr.slice(mid));
+
+  return merge(firstHalf, secondHalf, arr.length);
 }
+
+function merge<T>(firstHalf: T[], secondHalf: T[], n: number): T[] {
+  const sortedArr: T[] = [];
+
+  let i = 0,
+    j = 0,
+    k = 0;
+
+  while (k < n) {
+    if (
+      j >= secondHalf.length ||
+      (i < firstHalf.length && firstHalf[i] < secondHalf[j])
+    ) {
+      sortedArr[k] = firstHalf[i];
+      i++;
+    } else {
+      sortedArr[k] = secondHalf[j];
+      j++;
+    }
+    k++;
+  }
+
+  return sortedArr;
+}
+
+// cleaner implementation.
+// when no more listA.length o listB.length, push to the other one
+// function merge<T>(listA: T[], listB: T[]): T[] {
+
+//   const merged: T[] = [];
+
+//   while (listA.length && listB.length) {
+
+//       if (listA[0] <= listB[0])
+//           merged.push(listA.shift());
+//       else
+//           merged.push(listB.shift());
+//   }
+
+//   while (listA.length > 0) merged.push(listA.shift());
+//   while (listB.length > 0) merged.push(listB.shift());
+
+//   return merged;
+// }
